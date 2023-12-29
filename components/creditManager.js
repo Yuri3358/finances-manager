20e0861008fe04eabd0e27c42bd935fb572cb6fd
Yuri3358 <yurialmeida917@gmail.com>
2023-12-28 21:07:11 -0300

    feat: credit manager

--- components/creditManager.js
+++ components/creditManager.js
@@ -1,3 +1,22 @@
 const creditManager = {
-    template: creditManagerTemp
+    template: creditManagerTemp,
+    data() {
+        return {
+            newAmount: 0
+        }
+    }, 
+    methods: {
+        async setCredit() {
+            if (this.newAmount) {
+                const userID = localStorage.getItem("userId")
+                await colRef.doc(userID).update({
+                    credit: this.newAmount
+                })
+                this.newAmount = 0
+                alert("Quantia Registrada!")
+            } else {
+                alert("Quantia Inválida!")
+            }
+        }
+    }
 }
--- templates/creditManagerTemplate.js
+++ templates/creditManagerTemplate.js
@@ -2,9 +2,9 @@ const creditManagerTemp = `
     <section id="credit-container">
         <h1>Gestor de Saldo</h1>
         <label id="credit-label" for="credit-input">Novo saldo da conta</label>
-        <input id="credit-input" placeholder="R$" type="number">
+        <input id="credit-input" placeholder="R$" type="number" v-model="newAmount">
 
-        <button id="" class="btn btn-success">Registrar Saldo</button>
+        <button class="btn btn-success" @click="setCredit">Registrar Saldo</button>
 
     </section>
 `
