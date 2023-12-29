f9f632591fd8ee01d9290907e9cc246a4666181e
Yuri3358 <yurialmeida917@gmail.com>
2023-12-28 21:07:00 -0300

    feat: firebase auth integration

--- components/accountManager.js
+++ components/accountManager.js
@@ -1,5 +1,37 @@
 const accountManager = {
-    template: accountTemp
+    template: accountTemp,
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        async accountRegister() {
+            if (this.email && this.password) {
+                const credentials = await autho.createUserWithEmailAndPassword(this.email, this.password)
+                await colRef.doc(credentials.user.uid).set({
+                    credit: 0
+                })
+
+                await colRef.doc(credentials.user.uid).collection("transactions").add({})
+
+                window.location.href = "#/credit"
+            } else {
+                alert("Verifique as informações!")
+            }
+        },
+        async accountLogin() {
+            if (this.email && this.password) {
+                const loginData = await autho.signInWithEmailAndPassword(this.email, this.password)
+                localStorage.setItem("userId",loginData.user.uid)
+                
+                window.location.href = "#/credit"
+            } else {
+                alert("Verifique as informações!")
+            }
+        }
+    }
 }
 
 exports = { accountManager }
--- templates/accountTemplate.js
+++ templates/accountTemplate.js
@@ -3,13 +3,13 @@ const accountTemp = `
         <h2>Área da Conta</h2>
         
         <label class="account-labels" for="account-name">Email do Titular</label>
-        <input class="account-inputs" id="account-name">
+        <input class="account-inputs" id="account-name" v-model="email">
 
         <label class="account-labels" for="account-amount">Senha</label>
-        <input class="account-inputs" id="account-amount" type="password">
+        <input class="account-inputs" id="account-amount" type="password" v-model="password">
 
-        <button id="register-account" class="btn btn-success">Registrar</button>
-        <button id="enter-account" class="btn btn-warning">Entrar</button>
+        <button id="register-account" class="btn btn-success" @click="accountRegister">Registrar</button>
+        <button id="enter-account" class="btn btn-warning" @click="accountLogin">Entrar</button>
     </section>
 `
 
