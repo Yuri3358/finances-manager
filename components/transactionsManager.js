549c511fd4e28d513b044290503b62cb569809d1
Yuri3358 <yurialmeida917@gmail.com>
2023-12-29 11:59:34 -0300

    feat: transactions manager

--- components/transactionsManager.js
+++ components/transactionsManager.js
@@ -1,5 +1,52 @@
 const transactions = {
-    template: transactionsTemp
+    template: transactionsTemp,
+    data() {
+        return {
+            transaction: "",
+            transactionValue: 0,
+            transactionType: "",
+            date: ""
+        }
+    },
+    methods: {
+        async getCurrentWealth() {
+            const userID = localStorage.getItem("userId")
+            const docData = await colRef.doc(userID).get()
+            currentWealth = docData.data().credit
+
+            return currentWealth
+        },
+        async addTransaction() {
+            const userID = localStorage.getItem("userId")
+            
+            
+            if (this.transaction && this.transactionValue && this.transactionType) {
+                if (!this.date) {
+                    this.date = new Date().toISOString().slice(0, 10)
+                }
+                
+                this.transactionType == "Saída" ? this.transactionValue *= (-1) : this.transactionValue
+                
+                await colRef.doc(userID).collection("transactions").doc().set({
+                    transaction: this.transaction,
+                    value: this.transactionValue,
+                    type: this.transactionType,
+                    date: this.date
+                })
+
+                const currentWealth = await this.getCurrentWealth()
+                
+                await colRef.doc(userID).update({
+                    credit: currentWealth + this.transactionValue
+                })
+                
+                
+                this.transaction = this.transactionValue = this.transactionType = this.date = ""
+            } else {
+                alert("Verifique as Informações!")
+            }
+        }
+    }
 }
 
 exports = { transactions }
--- templates/transactionsTemplate.js
+++ templates/transactionsTemplate.js
@@ -2,19 +2,21 @@ const transactionsTemp = `
         <section id="transactions-container">
         <h2>Gestor de Movimentações</h2>
         <label class="transactions-labels" for="transaction-name">Nome da Movimentação</label>
-        <input class="transactions-inputs" id="transaction-name" placeholder="ex.: empréstimo, lanche...">
+        <input class="transactions-inputs" id="transaction-name" placeholder="ex.: empréstimo, lanche..." v-model="transaction">
 
         <label class="transactions-labels" for="transaction-amount">Valor da Movimentação</label>
-        <input class="transactions-inputs" id="transaction-amount" placeholder="R$">
+        <input class="transactions-inputs" id="transaction-amount" placeholder="R$" type="number" v-model="transactionValue">
+
+        <label class="transactions-labels" for="transaction-date">Data da Transação</label>
+        <input type="date" class="transactions-inputs" id="transaction-date" v-model="date">
 
         <label class="transactions-labels" for="transaction-type">Tipo de Movimentação</label>
-        <select class="transaction-inputs" id="transaction-type">
+        <select class="transaction-inputs" id="transaction-type" v-model="transactionType">
             <option>Entrada</option>
             <option>Saída</option>
         </select>
 
-        <button id="register-transaction" class="btn btn-success">Registrar Movimentação</button>
-
+        <button id="register-transaction" class="btn btn-success" @click="addTransaction">Registrar Movimentação</button>
     </section>
 `
 
