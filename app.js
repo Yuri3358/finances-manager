87caf156f8d2a927f5450e56de38a03adbb92bce
Yuri3358 <yurialmeida917@gmail.com>
2023-12-29 13:36:51 -0300

    feat(routes): navigation guards

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const app = Vue.createApp({})
 
+function handleNavigation() {
+    if (!firebase.auth().currentUser) {
+        alert("Você precisa estar logado!")
+        return false
+    } else {
+        return true
+    }
+}
 const routes = [
     {path: "/", component: accountManager},
-    {path: "/credit", component: creditManager },
-    {path: "/transactions", component: transactions},
-    {path: "/statement", component: bankStatement},
+    {path: "/credit", component: creditManager, beforeEnter: () => handleNavigation()},
+    {path: "/transactions", component: transactions, beforeEnter: () => handleNavigation()},
+    {path: "/statement", component: bankStatement, beforeEnter: () => handleNavigation()},
     {path: "/account", component: accountManager}
 ]
 
